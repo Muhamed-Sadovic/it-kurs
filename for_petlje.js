bf763a57f85a58918be60ae87f3c71b7feea0cf3
Muhamed Sadovic <muhamedsadovic41@gmail.com>
2021-08-11 14:55:04 +0200

    Vezbanje sa for petljom

---
+++ for_petlje.js
@@ -0,0 +1,31 @@
+/* 
+(let i=1;i<11;i++){
+    if(i === 7){
+        continue
+    }
+    console.log(i);
+} */
+
+/* 
+var suma = 0
+
+for(let i=0;i<=20;i+=2){
+    suma+=i
+console.log("Nakon broja "+ i + " " + "zbir je " + suma);
+} */
+
+/* for(i = 0; i < 6; i++){
+    if(i>2){
+        console.log("je super");
+    }
+    else{
+    console.log("python");
+    }
+} */
+
+var n = 100
+for(i = 0; i <= n; i++){
+    if(n % i === 0){
+        console.log(i);
+    }
+}
