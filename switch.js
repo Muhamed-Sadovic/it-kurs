f6cff2bd8bae432479526637a9381e2d450ab603
Muhamed Sadovic <muhamedsadovic41@gmail.com>
2021-08-11 14:53:12 +0200

    Vezbanje sa switch

---
+++ switch.js
@@ -0,0 +1,59 @@
+/*let a=8,b=3
+operacija = '/'
+
+switch(operacija){
+    case '+':
+    console.log(a+b);
+    break;
+
+    case '-':
+    console.log(a-b);
+    break
+
+    case '*':
+    console.log(a*b);
+    break
+
+    case '/':
+    console.log(a/b);
+    break
+
+    default:
+    console.log("Operacija nije validna")
+} 
+
+godina = 2004
+mesec = 2
+
+switch(mesec){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        console.log("Mesec ima 31 dan");
+        break
+
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        console.log("Mesec ima 30 dana");
+        break
+
+    case 2:
+        if(godina%4===0 && (godina%100!==0 || godina%400===0))
+        {
+            console.log("Mesec ima 29 dana")
+        }
+        else{
+            console.log("Mesec ima 28 dana")
+        }
+        break
+
+    default:
+        console.log("Mesec nije vazeci");
+}
+*/
