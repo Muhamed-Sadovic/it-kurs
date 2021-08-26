5dfcefbb7e70204cad376dd1e19b1f70e99e7541
Muhamed Sadovic <muhamedsadovic41@gmail.com>
2021-08-26 18:28:04 +0200

    domaci_8

---
+++ domaci8.js
@@ -0,0 +1,15 @@
+function countSmileys(arr) {
+    let brojac = 0
+    if(arr === ""){
+      return 0
+    }
+      for(let i=0;i<arr.length;i++){
+          if(arr[i] === ":)" || arr[i] === ";)" || arr[i] === ":D" || arr[i] === ";D" ||
+          arr[i] === ":-)" || arr[i] === ";-)" || arr[i] === ":~)" || arr[i] === ";~)" ||
+          arr[i] === ":-D" || arr[i] === ";-D" || arr[i] === ":~D" || arr[i] === ";~D"){
+          brojac++
+          }
+      }
+      return brojac
+    }
+    console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']));
